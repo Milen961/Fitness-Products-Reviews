455de09e61fd223f28aeeedb45f0b5c4426a10be
Milen961 <theheller178@gmail.com>
2023-03-23 18:14:47 +0200

    Add productService

---
+++ app/src/services/productService.js
@@ -0,0 +1,48 @@
+import { requestCommands } from "./requests";
+
+const baseUrl = 'http://localhost:3030/data/products';
+
+export const productServiceFactory = (token) => {
+    const request = requestCommands(token);
+
+    const getAll = async () => {
+        const result = await request.get(baseUrl);
+        const products = Object.values(result);
+    
+        return products;
+    };
+    
+    const getOne = async (productId) => {
+        const result = await request.get(`${baseUrl}/${productId}`);
+    
+        return result;
+    };
+    
+    const create = async (productData) => {
+        const result = await request.post(baseUrl, productData);
+    
+      
+    
+        return result;
+    };
+    
+    const addComment = async (productId, data) => {
+        const result = await request.post(`${baseUrl}/${productId}/comments`, data);
+    
+        return result;
+    };
+
+    const edit = (productId, data) => request.put(`${baseUrl}/${productId}`, data);
+
+    const deleteProduct = (productId) => request.delete(`${baseUrl}/${productId}`);
+
+
+    return {
+        getAll,
+        getOne,
+        create,
+        edit,
+        addComment,
+        delete: deleteProduct,
+    };
+}
