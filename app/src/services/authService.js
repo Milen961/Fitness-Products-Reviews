6e09abaff2ce912b410efeddfb6a6491858177db
Milen961 <theheller178@gmail.com>
2023-03-23 16:59:42 +0200

    Add authService

--- app/src/services/authService.js
+++ app/src/services/authService.js
@@ -4,7 +4,7 @@ const baseUrl = `http://localhost:3030/users`;
 
 export const authServiceUser = (token) => {
     const request = requestCommands(token);
-
+    
     return {
         login: (data) => request.post(`${baseUrl}/login`, data),
         register: (data) => request.post(`${baseUrl}/register`, data),
