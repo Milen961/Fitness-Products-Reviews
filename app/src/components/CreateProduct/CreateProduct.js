e0c7e6f5518aa8cf13b26e53e54519bea87acb92
Milen961 <theheller178@gmail.com>
2023-03-24 14:32:22 +0200

    Add a creat functionality

---
+++ app/src/components/CreateProduct/CreateProduct.js
@@ -0,0 +1,35 @@
+import { useForm } from '../../hooks/useForm';
+
+export const CreateProduct = ({
+    onCreateProductSubmit,
+}) => {
+    const { values, changeHandler, onSubmit } = useForm({
+        title: '',
+        category: '',
+        imageUrl: '',
+        summary: '',
+    }, onCreateProductSubmit);
+
+    return (
+        <section id="create-page" className="auth">
+            <form id="create" method="post" onSubmit={onSubmit}>
+                <div className="container">
+                    <h1>Create Review</h1>
+
+                    <label htmlFor="leg-title">Product:</label>
+                    <input value={values.title} onChange={changeHandler} type="text" id="title" name="title" placeholder="Enter product title" />
+
+                    <label htmlFor="category">Category:</label>
+                    <input value={values.category} onChange={changeHandler} type="text" id="category" name="category" placeholder="Enter product category" />
+
+                    <label htmlFor="game-img">Image:</label>
+                    <input value={values.imageUrl} onChange={changeHandler} type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo" />
+
+                    <label htmlFor="summary">Summary:</label>
+                    <textarea name="summary" id="summary" value={values.summary} onChange={changeHandler}></textarea>
+                    <input className="btn submit" type="submit" value="Create Review" />
+                </div>
+            </form>
+        </section>
+    );
+};
