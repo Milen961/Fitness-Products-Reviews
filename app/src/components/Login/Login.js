a3528283f2423b95416b0afb70487951fc1ba175
Milen961 <theheller178@gmail.com>
2023-03-28 15:03:16 +0300

    Add validation to Login/Register

--- app/src/components/Login/Login.js
+++ app/src/components/Login/Login.js
@@ -1,51 +1,76 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
-import { AuthContext } from "../../contexts/AuthContext";
 import { useForm } from "../../hooks/useForm";
-
-const LoginFormKeys = {
-    Email: 'email',
-    Password: 'password'
-};
+import { AuthContext } from "../../contexts/AuthContext";
 
 export const Login = () => {
-    const { onLoginSubmit } = useContext(AuthContext);
-    const { values, changeHandler, onSubmit } = useForm({
-        [LoginFormKeys.Email]: '',
-        [LoginFormKeys.Password]: '',
-    }, onLoginSubmit);
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const { onLoginSubmit } = useContext(AuthContext);
+  const { values, changeHandler, onSubmit } = useForm({
+    email: '',
+    password: '',
+  }, onLoginSubmit);
 
-    return (
-        <section id="login-page" className="auth">
-            <form id="login" method="POST" onSubmit={onSubmit}>
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        placeholder="Sokka@gmail.com"
-                        name={LoginFormKeys.Email}
-                        value={values[LoginFormKeys.Email]}
-                        onChange={changeHandler}
-                    />
+  const validateEmail = () => {
+    if (values.email.trim() === "") {
+      setEmailError("Email field is required");
+    } else {
+      setEmailError("");
+    }
+  };
 
-                    <label htmlFor="login-pass">Password:</label>
-                    <input
-                        type="password"
-                        id="login-password"
-                        name={LoginFormKeys.Password}
-                        value={values[LoginFormKeys.Password]}
-                        onChange={changeHandler}
-                    />
-                    <input type="submit" className="btn submit" value="Login" />
-                    <p className="field">
-                        <span>If you don't have profile click <Link to="/register">here</Link></span>
-                    </p>
-                </div>
-            </form>
-        </section>
-    );
-}
+  const validatePassword = () => {
+    if (values.password.length < 6) {
+      setPasswordError("Password must be at least 6 characters long");
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  return (
+    <section id="login-page" className="auth">
+      <form className="Login" id="login" method="POST" onSubmit={onSubmit}>
+        <div className="container">
+          <div className="brand-logo"></div>
+          <h1>Login</h1>
+
+          <label htmlFor="email">
+            <i className="fas fa-envelope"></i>
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="milen_skrin@abv.bg"
+            name="email"
+            value={values.email}
+            onBlur={validateEmail}
+            onChange={changeHandler}
+          />
+          {emailError && <span className="error">{emailError}</span>}
+
+          <label htmlFor="login-password">
+            <i className="fas fa-lock"></i>
+            Password:
+          </label>
+          <input
+            type="password"
+            id="login-password"
+            name="password"
+            value={values.password}
+            onBlur={validatePassword}
+            onChange={changeHandler}
+          />
+          {passwordError && <span className="error">{passwordError}</span>}
+
+          <input type="submit" className="btn submit" value="Login" />
+          <p className="field">
+            <span>If you don't have profile click <Link to="/register">here</Link></span>
+          </p>
+        </div>
+      </form>
+    </section>
+  );
+};
--- app/src/components/Register/Register.js
+++ app/src/components/Register/Register.js
@@ -1,61 +1,80 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { useForm } from "../../hooks/useForm";
 import { AuthContext } from "../../contexts/AuthContext";
 
 export const Register = () => {
-    const { onRegisterSubmit } = useContext(AuthContext);
-    const { values, changeHandler, onSubmit } = useForm({
-        email: '',
-        password: '',
-        confirmPassword: '',
-    }, onRegisterSubmit);
+  const [passwordError, setPasswordError] = useState("");
+  const { onRegisterSubmit } = useContext(AuthContext);
+  const { values, changeHandler, onSubmit } = useForm({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  }, onRegisterSubmit);
 
-    return (
-        <section id="register-page" className="content auth">
-        <form id="register" method="post" onSubmit={onSubmit}>
-          <div className="container">
-            <div className="brand-logo"></div>
-            <h1>Register</h1>
-      
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="milen_skrin@abv.bg"
-              value={values.email}
-              onChange={changeHandler}
-            />
-      
-            <label htmlFor="pass">Password:</label>
-            <input
-              type="password"
-              name="password"
-              id="register-password"
-              value={values.password}
-              onChange={changeHandler}
-            />
-      
-            <label htmlFor="con-pass">Confirm Password:</label>
-            <input
-              type="password"
-              name="confirmPassword"
-              id="confirm-password"
-              value={values.confirmPassword}
-              onChange={changeHandler}
-            />
-      
-            <input className="btn submit" type="submit" value="Register" />
-      
-            <p className="field">
-              <span>If you already have profile click <Link to="/login">here</Link></span>
-            </p>
-          </div>
-        </form>
-      </section>
-      
+  const validatePassword = () => {
+    if (values.password.length < 6) {
+      setPasswordError("Password must be at least 6 characters long");
+    } else {
+      setPasswordError("");
+    }
+  };
 
-    );
+  const validateConfirmPassword = () => {
+    if (values.password !== values.confirmPassword) {
+      setPasswordError("Passwords do not match");
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  return (
+    <section id="register-page" className="content auth">
+      <form className="Register" id="register" method="post" onSubmit={onSubmit}>
+        <div className="container">
+          <div className="brand-logo"></div>
+          <h1>Register</h1>
+
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="milen_skrin@abv.bg"
+            value={values.email}
+            onChange={changeHandler}
+          />
+
+          <label htmlFor="pass">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="register-password"
+            value={values.password}
+            onBlur={validatePassword}
+            onChange={changeHandler}
+          />
+          {passwordError && <span className="error">{passwordError}</span>}
+
+          <label htmlFor="con-pass">Confirm Password:</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirm-password"
+            value={values.confirmPassword}
+            onBlur={validateConfirmPassword}
+            onChange={changeHandler}
+          />
+          {passwordError && <span className="error">{passwordError}</span>}
+
+          <input className="btn submit" type="submit" value="Register" />
+
+          <p className="field">
+            <span>If you already have profile click <Link to="/login">here</Link></span>
+          </p>
+        </div>
+      </form>
+    </section>
+  );
 };
