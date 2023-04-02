c5cd65a9ab8279e1002bc41c51353e705a9b31e6
Milen961 <theheller178@gmail.com>
2023-04-02 23:26:56 +0300

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,12 @@
-import { Catalog } from "./components/Catalog/CatalogItem/CatalogItem";
+import { Catalog } from "./components/Catalog/CatalogItem/Catalog";
 import { Home } from "./components/Home/Home";
-import { useNavigate, Route, Routes } from "react-router-dom";
-import { AuthContext } from "./contexts/AuthContext";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { AuthContext, AuthProvider } from "./contexts/AuthContext";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
+
 import { authServiceUser } from "./services/authService";
 import { useState, useEffect } from "react";
 import { productServiceFactory } from "./services/productService";
@@ -13,107 +14,42 @@ import { Logout } from "./components/Logout/Logout";
 import { CreateProduct } from "./components/CreateProduct/CreateProduct";
 import { ProductDetails } from "./components/ProductDetails/ProductDetails";
 import { EditProduct } from "./components/EditProduct.js/EditProduct";
+import { RouteGuard } from "./components/common/RouteGuard";
+import { ProductProvider } from "./contexts/ProductContext";
 
 
 function App() {
-  const navigate = useNavigate();
-  const [products, setProducts] = useState([]);
-  const [auth, setAuth] = useState({});
-  const authService = authServiceUser(auth.accessToken)
-  const productService = productServiceFactory (auth.accessToken)
 
-useEffect(() => {
-  productService.getAll()
-  .then(result =>{
-    setProducts(result)
-  })
-}, []);
+ 
 
-  const onLoginSubmit = async (data) => {
-    try {
-        const result = await authService.login(data);
-
-        setAuth(result);
-
-        navigate('/catalog');
-    } catch (error) {
-        console.log('There is a problem');
-    }
-};
-const onRegisterSubmit = async (values) => {
-  const { confirmPassword, ...registerData } = values;
-  if (confirmPassword !== registerData.password) {
-      return;
-  }
-
-  try {
-      const result = await authService.register(registerData);
-
-      setAuth(result);
-
-      navigate('/catalog');
-  } catch (error) {
-      console.log('There is a problem');
-  }
-};
- const onLogout = async () =>{
-  await authService.logout();
-
-  setAuth({});
- };
-
- const onCreateProductSubmit = async (data) => {
-  const newProduct = await productService.create(data);
-
-   setProducts(state => [...state, newProduct]);
-   navigate('/catalog')
- };
-
- const onProductEdit = async (values) => {
-  const result = await productService.edit(values._id, values)
-
-   setProducts(state => state.map(e => e._id === values._id ? result : e))
-   navigate(`/catalog/${values._id}`);
- }
+  return (
+    <AuthProvider>
+  <ProductProvider>
+      <div id="box">
+        <Header />
 
 
+        <main id="main-content">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/catalog' element={<Catalog />} />
+            <Route path='/catalog/:productId' element={<ProductDetails />} />
 
-  const contextValues = {
-    onLoginSubmit,
-    onRegisterSubmit,
-    onLogout,
-    userId: auth._id,
-    token: auth.accessToken,
-    userEmail: auth.email,
-    isAuthenticated: !!auth.accessToken,
-  }
-
-  return(
-    <AuthContext.Provider value={contextValues}>
-
-  <div id="box">
-    <Header/>
-     
-
-      <main id="main-content">
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/login' element= {<Login/>} />
-                <Route path='/register' element= {<Register/>} />
-                <Route path= '/logout' element= {<Logout/>} />
-                <Route path= '/create-product' element= {<CreateProduct onCreateProductSubmit={onCreateProductSubmit}/>}/>
-                <Route path= '/catalog' element= {<Catalog products={products}/>} />
-                <Route path= '/catalog/:productId' element={<ProductDetails/>} />
-                <Route path= '/catalog/:productId/edit' element={<EditProduct onProductEdit={onProductEdit}/>} />
-
-
-
-              </Routes>
-      </main>
-      <Footer />
-  </div>
-    </AuthContext.Provider>
-  )
+              <Route element= {<RouteGuard /> }>
+            <Route path='/logout' element={<Logout />} />
+            <Route path='/create-product' element={<CreateProduct  />}/>
+            <Route path='/catalog/:productId/edit' element={<EditProduct  />} />
+            </Route>
+   
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+      </ProductProvider>
+    </AuthProvider>
+  );
 
 }
 
