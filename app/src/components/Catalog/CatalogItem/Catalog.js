b3a8a255a2564f4b5d1a743e29b009de02e4f12a
Milen961 <theheller178@gmail.com>
2023-04-02 23:34:28 +0300

    Update catalog

--- app/src/components/Catalog/CatalogItem/Catalog.js
+++ app/src/components/Catalog/CatalogItem/Catalog.js
@@ -1,25 +1,46 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-export const CatalogItem = ({
-    _id,
-    title,
-    description,
-    imageUrl,
-    category,
-}) => {
+// export const CatalogItem = ({
+//     _id,
+//     title,
+//     description,
+//     imageUrl,
+//     category,
+// }) => {
+//     return (
+//         <div className="allProducts">
+//             <div className="allProducts-info">
+//                 <div className="product-image-container">
+//                     <img src={imageUrl} alt={`Product ${title}`} />
+//                 </div>
+//                 <div className="product-details">
+//                     <h2 className="product-title">{title}</h2>
+//                     <h3 className="product-category">{category}</h3>
+//                     <p className="product-description">{description}</p>
+//                     <Link to={`/catalog/${_id}`} className="details-button">Details</Link>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+import { useProductContext } from "../../../contexts/ProductContext";
+import { CatalogItem } from "./CatalogItem";
+
+export const Catalog = () => {
+    const { products } = useProductContext();
+
     return (
-        <div className="allProducts">
-            <div className="allProducts-info">
-                <div className="product-image-container">
-                    <img src={imageUrl} alt={`Product ${title}`} />
-                </div>
-                <div className="product-details">
-                    <h2 className="product-title">{title}</h2>
-                    <h3 className="product-category">{category}</h3>
-                    <p className="product-description">{description}</p>
-                    <Link to={`/catalog/${_id}`} className="details-button">Details</Link>
-                </div>
-            </div>
-        </div>
+        <section id="catalog-page">
+            <h1>All Products</h1>
+
+            {products.map(x =>
+                <CatalogItem key={x._id} {...x} />
+            )}
+
+            {products.length === 0 && (
+                <h3 className="no-articles">No articles yet</h3>
+            )}
+        </section>
     );
-}
+};
--- app/src/components/Catalog/CatalogItem/CatalogItem.js
+++ app/src/components/Catalog/CatalogItem/CatalogItem.js
@@ -1,19 +1,25 @@
-import { CatalogItem } from "./Catalog";
- 
-export const Catalog = ({
-    products,
+import { Link } from "react-router-dom";
+
+export const CatalogItem = ({
+    _id,
+    title,
+    description,
+    imageUrl,
+    category,
 }) => {
     return (
-        <section id="catalog-page">
-            <h1>All Products</h1>
-
-            {products.map(x =>
-                <CatalogItem key={x._id} {...x} />
-            )}
-
-            {products.length === 0 && (
-                <h3 className="no-articles">No articles yet</h3>
-            )}
-        </section>
+        <div className="allProducts">
+            <div className="allProducts-info">
+                <div className="product-image-container">
+                    <img src={imageUrl} alt={`Product ${title}`} />
+                </div>
+                <div className="product-details">
+                    <h2 className="product-title">{title}</h2>
+                    <h3 className="product-category">{category}</h3>
+                    <p className="product-description">{description}</p>
+                    <Link to={`/catalog/${_id}`} className="details-button">Details</Link>
+                </div>
+            </div>
+        </div>
     );
-};
+}
