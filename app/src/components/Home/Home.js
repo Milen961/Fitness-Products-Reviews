551998974606d114e809bcc700593a69de5adec7
Milen961 <theheller178@gmail.com>
2023-04-04 13:10:42 +0300

    Update Home.js

--- app/src/components/Home/Home.js
+++ app/src/components/Home/Home.js
@@ -1,57 +1,37 @@
-export const Home = () => {
-    return (
-      <section id="welcome-world">
-        <div className="welcome-message">
-          <h2>Welcome to FitReviews</h2>
-          <h3>Your go-to source for fitness product reviews</h3>
-        </div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyBRptfp7P9FJstotLpQIZQGysDaw6JWPJFZwUb-uI-9v8BhaajzTToELqcJfm6rDOHu0&usqp=CAU" alt="product" />
-  
-        <div id="home-page">
-          <h1>Latest Reviews</h1>
-  
-          {/* Display div with information about every product (if any) */}
-          <div className="product">
-            <div className="image-wrap">
-              <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2023%2F03%2F03%2FWhat-Happens-to-Your-Body-When-You-Take-Protein-Powder-Every-Day.jpg" alt="product image" />
-            </div>
-            <h3>Protein Powder</h3>
-            <div className="rating">
-              <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-              <a href="#" className="btn details-btn">Details</a>
-            </div>
-          </div>
-  
-          <div className="product">
-            <div className="image-wrap">
-              <img src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1663005594-hovr-infinite-4-1663005562.jpg?crop=1xw:1xh;center,top&resize=980:*" alt="shoe image" />
-            </div>
-            <h3>Workout Shoes</h3>
-            <div className="rating">
-              <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-              <a href="#" className="btn details-btn">Details</a>
-            </div>
-          </div>
-  
-          <div className="product">
-            <div className="image-wrap">
-              <img src="https://cdn.mos.cms.futurecdn.net/E6ADsCBuwyLiEb5XCGq2XX.jpg" alt="tracker" />
-            </div>
-            <h3>Fitness Tracker</h3>
-            <div className="rating">
-              <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-              <a href="#" className="btn details-btn">Details</a>
-            </div>
-          </div>
+import { useEffect, useCallback, useState, useMemo } from "react";
+import LatestReview from "./LatestReviews"
 
-        </div>
+export const Home = () => {
+  const [latestReviews, setLatestReviews] = useState([]);
+
+  useEffect(() => {
+      fetch(`http://localhost:3030/data/products`)
+          .then(res => res.json())
+          .then(result => {
+            setLatestReviews(result.map(x => ({ ...x, rating: 0 })));
+          });
+  }, []);
+
+  const onLikeClick = useCallback((productId) => {
+    setLatestReviews(state => state.map(x => x._id ===  productId ? { ...x, rating: x.rating + 1 } : x));
+  }, [])
+
+  return (
+      <section id="welcome-world">
+
+          <div className="welcome-message">
+              {/* <h2>ALL new Review are</h2> */}
+          </div>
+          <img src="https://img.freepik.com/premium-vector/gym-logo-design-template-fitness-club-creative-symbols_487414-2141.jpg?w=2000" alt="fit" />
+
+          <div id="home-page">
+              <h1>Latest Reviews </h1>
+
+              {latestReviews.map(product => <LatestReview key={product._id} {...product} onLikeClick={onLikeClick} />)}
+
+              {latestReviews.length === 0 && <p className="no-articles">No reviews yet</p>}
+          </div>
       </section>
-    );
+  );
   }
   
