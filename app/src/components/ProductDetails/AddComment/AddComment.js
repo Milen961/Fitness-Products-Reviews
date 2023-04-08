1450f723a97b987cb9275f3a8da654679fc2b236
Milen961 <theheller178@gmail.com>
2023-04-08 17:02:23 +0300

    Update comment field so it can not be sent empty

--- app/src/components/ProductDetails/AddComment/AddComment.js
+++ app/src/components/ProductDetails/AddComment/AddComment.js
@@ -1,19 +1,39 @@
-import { useForm } from "../../../hooks/useForm";
+import { useState } from 'react';
+import { useForm } from '../../../hooks/useForm';
 
-export const AddComment = ({
-    onCommentSubmit,
-}) => {
-    const { values, changeHandler, onSubmit } = useForm({
-        comment: ''
-    }, onCommentSubmit);
+export const AddComment = ({ onCommentSubmit }) => {
+  const { values, changeHandler, onSubmit } = useForm(
+    {
+      comment: '',
+    },
+    onCommentSubmit
+  );
 
-    return (
-        <article className="create-comment">
-            <label>Add new comment:</label>
-            <form className="form" onSubmit={onSubmit}>
-                <textarea name="comment" placeholder="Comment......" value={values.comment} onChange={changeHandler}></textarea>
-                <input className="btn submit" type="submit" value="Add Comment" />
-            </form>
-        </article>
-    );
+  const [commentError, setCommentError] = useState('');
+
+  const validateComment = () => {
+    if (values.comment.trim() === '') {
+      setCommentError('Comment is required');
+    } else {
+      setCommentError('');
+    }
+  };
+
+  return (
+    <article className="create-comment">
+      <label>Add new comment:</label>
+      <form className="form" onSubmit={onSubmit}>
+        <textarea
+          name="comment"
+          placeholder="Comment..."
+          value={values.comment}
+          onChange={changeHandler}
+          onBlur={validateComment}
+          required
+        />
+        {commentError && <span className="error">{commentError}</span>}
+        <input className="btn submit" type="submit" value="Add Comment" />
+      </form>
+    </article>
+  );
 };
