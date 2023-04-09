f3bac1cfa939f67faa48de6bf3c311c0a679eae6
Milen961 <theheller178@gmail.com>
2023-04-09 11:25:10 +0300

    Update app.js /style.css

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,18 @@
 import { Catalog } from "./components/Catalog/CatalogItem/Catalog";
 import { Home } from "./components/Home/Home";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { AuthContext, AuthProvider } from "./contexts/AuthContext";
+import { Route, Routes } from "react-router-dom";
+import {  AuthProvider } from "./contexts/AuthContext";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
-
-import { authServiceUser } from "./services/authService";
-import { useState, useEffect } from "react";
-import { productServiceFactory } from "./services/productService";
 import { Logout } from "./components/Logout/Logout";
 import { CreateProduct } from "./components/CreateProduct/CreateProduct";
 import { ProductDetails } from "./components/ProductDetails/ProductDetails";
 import { EditProduct } from "./components/EditProduct.js/EditProduct";
 import { RouteGuard } from "./components/common/RouteGuard";
 import { ProductProvider } from "./contexts/ProductContext";
+import { PageNotFound } from "./components/PageNotFound/PageNotFound";
 
 
 function App() {
@@ -42,7 +39,7 @@ function App() {
             <Route path='/create-product' element={<CreateProduct  />}/>
             <Route path='/catalog/:productId/edit' element={<EditProduct  />} />
             </Route>
-   
+            <Route path="*" element={<PageNotFound />} />
           </Routes>
         </main>
         <Footer />
