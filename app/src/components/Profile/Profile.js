e2d685ce730142c1d31f64f3a70ec1b6a62e19da
Milen961 <theheller178@gmail.com>
2023-12-04 11:45:04 +0200

    Fix profile requests to the server

---
+++ app/src/components/Profile/Profile.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { ProfileService } from '../../services/profileService';
+
+const Profile = ({ token, personId }) => {
+  const [profileService] = useState(() => ProfileService(token));
+  const [userProfile, setUserProfile] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await profileService.getOne(personId);
+        setUserProfile(result);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching user profile:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [profileService, personId]);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : userProfile ? (
+        <div>
+          <h1>User Profile</h1>
+          <p>Name: {userProfile.name}</p>
+          <p>Email: {userProfile.email}</p>
+        </div>
+      ) : (
+        <p>No user profile found.</p>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
